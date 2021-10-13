8d07fcdef9e32ae3feb1e03ebec9daad1f4006c5
mightybhavesh <37592402+mightybhavesh@users.noreply.github.com>
2021-10-13 15:43:07 +0530

    port update 2

--- OAuthFlows-master/Server.js
+++ OAuthFlows-master/Server.js
@@ -30,7 +30,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT );
 
 app.post('', function(req, res) {
 
