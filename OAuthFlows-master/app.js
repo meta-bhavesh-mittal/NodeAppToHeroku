bfdf5a8c022b94a893cb85e43edccd49f26ecd47
mightybhavesh <37592402+mightybhavesh@users.noreply.github.com>
2021-10-13 18:31:38 +0530

    Update

--- OAuthFlows-master/app.js
+++ OAuthFlows-master/app.js
@@ -31,6 +31,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 8080);
 
+/*Allow CORS*/
+app.use(function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+    res.setHeader('Access-Control-Max-Age', '1000');
+
+    next();
+});
+
 app.post('', function(req, res) {
 
 });
