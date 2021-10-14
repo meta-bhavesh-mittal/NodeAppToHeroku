1bc35869e1239b02e43c0253b9007d2ba2de3daa
mightybhavesh <37592402+mightybhavesh@users.noreply.github.com>
2021-10-14 12:57:40 +0530

    Update 5

--- OAuthFlows-master/app.js
+++ OAuthFlows-master/app.js
@@ -37,6 +37,7 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization,x-content-type,x-user-agent,salesforceproxy-endpoint');
     res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Expose-Origin', '*');
     res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
     res.setHeader('Access-Control-Max-Age', '1000');
 
